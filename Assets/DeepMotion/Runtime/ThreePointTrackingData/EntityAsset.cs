8853b63f385579427779eb1003468ee98dacc404
Tristan Fogt <tristanfogt1418@hotmail.com>
2018-09-10 23:22:09 -0400

    Revert "Initial commit"
    This reverts commit 1359c0b523ed06aa49a20d46d35bb4f2ed339d4c.

--- Assets/DeepMotion/Runtime/ThreePointTrackingData/EntityAsset.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-[System.Serializable]
-[CreateAssetMenu(fileName = "Assets/Runtime/ThreePointTracking/Data/Entities/EntityAssetData", menuName = "ThreePointTracking Data/Entity")]
-public  class EntityAsset : PlayerAsset
-{
-	public Vector3 spawnLocation;
-	public Vector3 spawnRotation;
-}
--- Assets/DeepMotion/Runtime/ThreePointTrackingData/PlayerAsset.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-[System.Serializable]
-[CreateAssetMenu(fileName = "Assets/Runtime/ThreePointTracking/Data/PlayerAssetData", menuName = "ThreePointTracking Data/Player")]
-public  class PlayerAsset: ScriptableObject {
-    public GameObject networkEntityPrefab;
-    public GameObject clientAssetPrefab;
-    public GameObject localClientAssetPrefab;
-    public GameObject serverAssetPrefab;
-}
